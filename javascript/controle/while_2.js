5ec8bbb9ea5c5a5d59521590bd42850e7baf2f20
LeonardoGomes182 <dlcgomes182@gmail.com>
2024-07-05 13:45:31 -0300

    new files while

---
+++ javascript/controle/while_2.js
@@ -0,0 +1,5 @@
+let quantidade = 0;
+while(Math.random() < 0.9){
+  quantidade++
+}
+console.log('Interação ', quantidade)
---
+++ javascript/controle/while_3.js
@@ -0,0 +1,9 @@
+//quantidade Mili desde 1 de janeiro de 1970
+
+let quantidade = 0
+const dataNoFuturo = Date.now() + 3000
+
+while (Date.now() < dataNoFuturo){
+    quantidade++
+}
+console.log("Qtade: ", quantidade)
