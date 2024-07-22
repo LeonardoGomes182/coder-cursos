8ea3fc7e49581c824db956a05636e84aa9aec690
LeonardoGomes182 <dlcgomes182@gmail.com>
2024-07-22 16:48:50 -0300

    created new folder and files arrays

---
+++ evoluindo/arrays/basico_array_1.js
@@ -0,0 +1,12 @@
+let notas = [7.8, 7.3, 5.8, 9.8, 8.3, 10];
+
+console.log(notas);
+console.log(notas[3]);
+
+notas[2] = 10;
+
+console.log(notas[2])
+
+notas[6] = 10;
+
+console.log(notas[6])
---
+++ evoluindo/arrays/basico_array_3.js
@@ -0,0 +1,11 @@
+const numeros = [7,8,9]
+
+console.log(numeros)
+
+numeros[3] = 10;
+console.log(numeros)
+
+numeros.push(11)
+
+numeros.push( 12, 13,14)
+console.log(numeros.length)
