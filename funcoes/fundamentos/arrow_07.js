f092ad9164a48f7e6ea0c89384fff6c28605749e
LeonardoGomes182 <dlcgomes182@gmail.com>
2024-09-12 21:37:54 -0300

    new files arrow

--- funcoes/fundamentos/arrow_07.js
+++ funcoes/fundamentos/arrow_07.js
@@ -16,8 +16,15 @@ console.log(subtrair(10, 5))
 
 
 
-const bomDia = ()=> 'Bom dia'
+const bomDia=()=>"Bom dia!!!!!!!"
 console.log(bomDia())
 
 const boaTarde = _ => 'Boa tarde' //só pode remover o par de chaves neste caso, se colocar um parametro para igonorar
 console.log(boaTarde())
+
+
+const subtrair2 = (a,b)=> a-b
+console.log(subtrair2(20,8))
+
+const multiplicar = (b, c)=> b*c;
+console.log(multiplicar(10,10))
---
+++ funcoes/fundamentos/desafio.js
@@ -0,0 +1,33 @@
+// fn -> 3*7
+const multiplicar = (a, b) => a * b;
+//fn -> 3+7
+const somar = (a, b) => a + b;
+
+//fn -. 3-7
+const subtrair = (a, b) => a - b;
+
+//calcular(3)(7)(fn)
+const calcular = a => b => fn => fn(a, b);
+
+console.log(calcular(10)(7)(multiplicar));
+console.log(calcular(10)(7)(somar));
+console.log(calcular(10)(7)(subtrair));
+
+//--------------TROCANDO A ORDEM DA CHAMADA------------------------------
+
+const calcular1 = fn => a => b => fn(a, b);
+
+console.log(calcular1(multiplicar)(10)(7));
+console.log(calcular1(somar)(10)(7));
+console.log(calcular1(subtrair)(10)(7));
+
+//--------------funcao não reduzida------------------------------
+// const calcular = (a) => {
+//   return (b) => {
+//     return (fn) => {
+//         return fn(a,b)
+//     };
+//   };
+// };
+
+
