cafdcb9f09298de82e79d106333db9b6f1c53748
LeonardoGomes182 <dlcgomes182@gmail.com>
2024-10-10 22:17:19 -0300

    new files objeto

---
+++ funcoes/oo/objeto_literal_02.js
@@ -0,0 +1,18 @@
+//Funções e POO - JS aula 53
+
+const produto = {
+  nome: "iPad",
+  marca: "Apple",
+  anoFabricacao: 2023,
+  preco: 7500,
+  fragil: true,
+  desconto: 0.15,
+
+};
+
+function precoComDesconto(produto) {
+  return (1 - produto.desconto) * produto.preco;
+}
+
+const precoFinal = precoComDesconto(produto);
+console.log(precoFinal)
