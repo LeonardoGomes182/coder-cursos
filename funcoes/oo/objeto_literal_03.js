8ec807d58c4d57fb572b99e3cd0466af0245c72f
LeonardoGomes182 <dlcgomes182@gmail.com>
2024-10-17 23:04:10 -0300

    new file objeto

---
+++ funcoes/oo/objeto_literal_03.js
@@ -0,0 +1,16 @@
+//Funções e POO - JS aula 54
+
+//O problema da utilização de Objetos Literais
+
+const usuário = {
+  nome: "Ana",
+  email: "ana@ana.com.br",
+  senha:'123456',
+  cadastro: new Date(), //funcao construtora
+};
+
+const usuário2 = {
+  nome: "Carlos",
+  email: "carlos@carlos.com.br",
+  dataCadastro: new Date(),
+};
