0570091d4d0456ba885760f502efcddb85b73519
LeonardoGomes182 <dlcgomes182@gmail.com>
2025-07-10 15:43:03 -0300

    revisao

---
+++ revisao/funcoes/com_uso_funcao_01.js
@@ -0,0 +1,25 @@
+const d1 = 7;
+const m1 = "Maio";
+const a1 = 2022;
+
+function exibirData1() {
+  console.log(`Dia: ${d1}`);
+  console.log(`Mes: ${m1}`);
+  console.log(`Ano: ${a1}`);
+  console.log(`${d1}/${m1}/${a1}`);
+  console.log(`${d1} de ${m1} de ${a1}`);
+}
+
+const d2 = 12;
+const m2 = "Dezembro";
+const a2 = 2024;
+
+function exibirData2() {
+  console.log(`Dia: ${d2}`);
+  console.log(`Mes: ${m2}`);
+  console.log(`Ano: ${a2}`);
+  console.log(`${d2}/${m2}/${a2}`);
+}
+
+exibirData1();  
+exibirData2();
---
+++ revisao/funcoes/sem_uso_funcao.js
@@ -0,0 +1,18 @@
+const d1 = 7;
+const m1 = "Maio";
+const a1 = 2022;
+
+console.log(`Dia: ${d1}`)
+console.log(`Mes: ${m1}`)
+console.log(`Ano: ${a1}`)
+console.log(`${d1}/${m1}/${a1}`)
+console.log(`${d1} de ${m1} de ${a1}`)
+
+const d2 = 12;
+const m2 = "Dezembro";
+const a2 = 2024;
+
+console.log(`Dia: ${d2}`)
+console.log(`Mes: ${m2}`)
+console.log(`Ano: ${a2}`)
+console.log(`${d2}/${m2}/${a2}`)
